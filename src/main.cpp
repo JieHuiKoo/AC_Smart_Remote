598da883bc8c0d53fd5b5beb7197dd990145c008
JieHuiKoo <31171083+JieHuiKoo@users.noreply.github.com>
2021-08-26 18:39:56 +0800

    Added temperature off and on trigger

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <dht.h>
 #include <Servo.h>
 
-unsigned long previousMillis = 0;
+// Reset led and button states
 int led1_state = 0;
 int led2_state = 0;
 int led3_state = 0;
@@ -10,7 +10,12 @@ int led4_state = 0;
 int led5_state = 0;
 int led6_state = 0;
 
-Servo servo;
+int button1_state = 0;
+int button2_state = 0;
+int button3_state = 0;
+int button4_state = 0;
+
+// Declare pin numbers
 int servo_pin = 10;
 
 int led6_pin = 21;
@@ -24,25 +29,31 @@ int button1_pin = 27;
 int button2_pin = 0;
 int button3_pin = 1;
 int button4_pin = 5;
-
 int dht_pin = 45;
-DHT dht(dht_pin, DHT11);
-int hum = 0;
-int temp = 0;
 
-int on_hum = 0;
-int on_temp = 0;
-int off_hum = 0;
-int off_temp = 0;
-int on_interval = 0;
-int off_interval = 0;
-
-int button1_state = 0;
-int button2_state = 0;
-int button3_state = 0;
-int button4_state = 0;
-
+// Declare trigger variables
+float hum = 0;
+float temp = 0;
+float on_hum = 0;
+float on_temp = 0;
+float off_hum = 0;
+float off_temp = 0;
+unsigned long on_interval = 0;
+unsigned long off_interval = 0;
 int mode = 0;
+int ac_state = 0;
+
+// Acceptable trigger variable range
+float hum_interval = 2;
+float temp_interval = 0.5;
+
+// For blinking LED
+unsigned long previousMillis = 0;
+int blink_count = 0;
+
+// Declare Servo and Humidity sensor objects
+Servo servo;
+DHT dht(dht_pin, DHT11);
 
 void setup() {
   Serial.begin(9600);
@@ -87,6 +98,8 @@ void trigger_button()
   delay(230);
   servo.write(90);
   delay(500);
+
+  ac_state = !ac_state;
 }
 
 void led_toggle(int led1_flag, int led2_flag, int led3_flag, int led4_flag, int led5_flag, int led6_flag)
@@ -129,19 +142,25 @@ void settings_confirm()
 
 void temp_sense()
 {
-  //Read data and store it to variables hum and temp
-    hum = dht.readHumidity();
-    temp= dht.readTemperature();
-    //Print temp and humidity values to serial monitor
-    Serial.print("Humidity: ");
-    Serial.print(hum);
-    Serial.print(" %, Temp: ");
-    Serial.print(temp);
-    Serial.println(" Celsius");
-    delay(2000); //Delay 2 sec.
-    led_toggle(0,0,0,0,0,0);
-    delay(100);
-    led_toggle(1,1,1,1,1,1);
+  int number_of_samples = 6;
+  int led_array[6] = {0,0,0,0,0,0};
+
+  hum = 0;
+  temp = 0;
+  
+  for (int i = 0; i<number_of_samples; i++)
+  {
+    hum = hum + dht.readHumidity();
+    temp = temp + dht.readTemperature();
+    led_array[i] = 1;
+    
+    delay(1000); //Delay 1 sec.
+    led_toggle(led_array[5], led_array[4], led_array[3], led_array[2], led_array[1], led_array[0]);
+  }
+  
+  // Find average of 6 samples
+  hum = hum/number_of_samples;
+  temp = temp/number_of_samples;
 }
 
 void reset_led_state()
@@ -154,10 +173,11 @@ void reset_led_state()
   led6_state = 0;
 }
 
-unsigned long blink_led(unsigned long currentMillis, unsigned long interval, int led1_toggle, int led2_toggle, int led3_toggle, int led4_toggle, int led5_toggle, int led6_toggle )
-{    
+void blink_led(unsigned long currentMillis, unsigned long interval, int led1_toggle, int led2_toggle, int led3_toggle, int led4_toggle, int led5_toggle, int led6_toggle )
+{ 
     if (currentMillis - previousMillis >= interval) 
     {
+      blink_count++;
       // save the last time you blinked the LED
       previousMillis = currentMillis;
 
@@ -188,7 +208,7 @@ unsigned long blink_led(unsigned long currentMillis, unsigned long interval, int
 
       // Send control signal to led
       led_toggle(led1_state, led2_state, led3_state, led4_state, led5_state, led6_state);
-    }
+  }
 }
 
 int obtain_mode()
@@ -204,7 +224,8 @@ int obtain_mode()
 
   while (flag)
   {
-    blink_led(millis(), 1000, 1, 1, 1, 1, 1, 1);
+    // Note: Remember to reset blink count
+    blink_led(millis(), 500, 1, 1, 1, 1, 1, 1);
 
     if (!digitalRead(button1_pin))
     {
@@ -215,6 +236,8 @@ int obtain_mode()
       off_temp = 0;
       on_interval = 0;
       off_interval = 0;
+
+      blink_count = 0;
       return 1;
     }
     if (!digitalRead(button2_pin))
@@ -226,6 +249,8 @@ int obtain_mode()
       off_temp = 0;
       on_interval = 0;
       off_interval = 0;
+
+      blink_count = 0;
       return 2;
     }
     if (!digitalRead(button3_pin))
@@ -237,9 +262,12 @@ int obtain_mode()
       off_temp = 0;
       on_interval = 0;
       off_interval = 0;
+
+      blink_count = 0;
       return 3;
     }
   }
+  return 0;
 }
 
 void loop()
@@ -255,88 +283,106 @@ void loop()
   else if (mode == 1)
   {
     // LED indicator for mode
-    blink_led(millis(), 500, 1,0,0,0,0,0);
 
-    // Trigger to exit mode to change mode
-    if (!digitalRead(button1_pin) && !digitalRead(button4_pin))
+    while (blink_count <= 5)
+      blink_led(millis(), 500, 1, 0, 0, 0, 0, 0);
+    reset_led_state();
+
+    // Set Trigger Parameters for Turning on
+    while (on_temp == 0 || on_hum == 0)
     {
-      mode = obtain_mode();
+      blink_led(millis(), 500, 1, 1, 0, 0, 0, 0);
+      
+      if (!digitalRead(button1_pin) && !digitalRead(button2_pin))
+      {
+        led_toggle(1,1,0,0,0,0);
+        temp_sense();
+        on_hum = hum;
+        on_temp = temp;
+        Serial.println("On Humidity: " + String(on_hum) + "\nOn Temp: " + String(on_temp) + "\n\n");
+        trigger_button();
+        reset_led_state();
+      }
+    }
+
+    // Set Trigger Parameters for Turning off
+    while (off_temp == 0 || off_hum == 0)
+    {
+      // Make led 1 and 2 stay on while blinking led 5 and led 6
+      led1_state = 1;
+      led2_state = 1;
+      blink_led(millis(), 500, 0, 0, 0, 0, 1, 1);
+
+      if (!digitalRead(button3_pin) && !digitalRead(button4_pin))
+      {
+        led_toggle(1,1,0,0,1,1);
+        temp_sense();
+        off_hum = hum;
+        off_temp = temp;
+        Serial.println("On Humidity: " + String(off_hum) + "\nOn Temp: " + String(off_temp) + "\n\n");
+        trigger_button();
+        reset_led_state();
+      }
+
+    }
+
+    while(mode == 1)
+    {
+      // Trigger to exit mode to change mode
+      if (!digitalRead(button1_pin) && !digitalRead(button4_pin))
+      {
+        blink_count=0;
+        while (blink_count <= 20)
+          blink_led(millis(), 100, 1, 1, 1, 1, 1, 1);
+        mode = 0;
+        break;
+      }
+
+      temp_sense();
+
+      Serial.println("AC Status: " + String(ac_state) + "\nOn Humidity:      " + String(on_hum) + " On Temp:      " + String(on_temp) + "\nOff Humidity:     " + String(off_hum) + " Off Temp:     " + String(off_temp) + "\nCurrent Humidity: "+ String(hum) + " Current Temp: "+ String(temp) + "\n\n");
+      
+      if (ac_state == 1)
+      {
+        if ((abs(hum-off_hum) < hum_interval) || (abs(temp-off_temp) < temp_interval) || temp < off_temp || hum < off_hum)
+        {
+          trigger_button();
+        }
+      }
+      else if (ac_state == 0)
+      {
+        if ((abs(hum-on_hum) < hum_interval) || (abs(temp-on_temp) < temp_interval) || temp > on_temp || hum > on_hum)
+        {
+          trigger_button();
+        }
+      }
     }
   }
 
   else if (mode == 2)
   {
-    // LED indicator for mode
-    blink_led(millis(), 500, 0,1,0,0,0,0);
-
+    // LED indicator for mode  Note: Reset blink count
+    while (blink_count <= 5)
+      blink_led(millis(), 500, 0, 1, 0, 0, 0, 0);
+    blink_count = 0;
     // Trigger to exit mode to change mode
     if (!digitalRead(button1_pin) && !digitalRead(button4_pin))
     {
-      mode = obtain_mode();
+      mode = 0;
     }
   }
 
   else if (mode == 3)
   {
-    // LED indicator for mode
-    blink_led(millis(), 500, 0,0,1,0,0,0);
+    // LED indicator for mode  Note: Reset blink count
+    while (blink_count <= 5)
+      blink_led(millis(), 500, 0, 0, 1, 0, 0, 0);
+    blink_count = 0;
 
     // Trigger to exit mode to change mode
     if (!digitalRead(button1_pin) && !digitalRead(button4_pin))
     {
-      mode = obtain_mode();
+      mode = 0;
     }
   }
 }
-
-
-
-
-
-// //
-// delay(100);
-//   // Initialize leds to off
-//   led_toggle(0,0,0,0,0,0);
-  
-//   if (!digitalRead(button1_pin))
-//   {
-//     digitalWrite(led1_pin, HIGH);
-//     // print_line("1 press");
-//   }
-//   else if (!digitalRead(button2_pin))
-//   {
-//     digitalWrite(led2_pin, HIGH);
-//     // print_line("2 press");
-//   }
-//   else if (!digitalRead(button3_pin))
-//   {
-//     digitalWrite(led3_pin, HIGH);
-//     // print_line("3 press");
-//   }
-//   else if (!digitalRead(button4_pin))
-//   {
-//     digitalWrite(led4_pin, HIGH);
-//     // print_line("4 press");
-//   }
-//   if (!digitalRead(button1_pin) && !digitalRead(button4_pin))
-//   {
-//     settings_confirm();
-//     temp_sense();
-//     led_toggle(1,1,1,1,1,1);
-    
-//     // print_line("1 and 4 press");
-//   }
-//   if (!digitalRead(button1_pin) && !digitalRead(button2_pin))
-//   {
-//     turn_on_led_confirm();
-//     temp_sense();
-//     trigger_button();
-//     // print_line("1 and 2 press");
-//   }
-//   if (!digitalRead(button3_pin) && !digitalRead(button4_pin))
-//   {
-//     turn_off_led_confirm();
-//     temp_sense();
-//     trigger_button();
-//     // print_line("3 and 4 press");
-//   }
